9482ef44e43b84aeb5101df8d97539833473ad10
Vishal Gautam <ivishalgautamm@gmail.com>
2025-01-09 15:41:21 +0530

    some ui changes

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,15 +1,11 @@
 import React, { lazy, Suspense } from "react";
 const Navbar = lazy(() => import("../../components/navbar"));
 const Footer = lazy(() => import("../footer"));
-import { ScrollArea } from "../ui/scroll-area";
-import { popularUnlistedShares } from "@/data";
-import { rupee } from "@/hooks/Intl";
-import { cn } from "@/lib/utils";
 import SharesMarquee from "../shares-marquee";
 
 export default function Layout({ children }) {
   return (
-    <div className="h-screen bg-gray-50">
+    <div className="bg-gray-50">
       <Suspense fallback={<div>loading...</div>}>
         <Navbar />
       </Suspense>
--- components/shares-marquee.jsx
+++ components/shares-marquee.jsx
@@ -5,57 +5,30 @@ import React from "react";
 
 export default function SharesMarquee() {
   return (
-    <marquee behavior="" direction="" className="bg-black flex">
-      <div className="h-10 flex gap-10 items-center">
-        {popularUnlistedShares.map((item, ind) => (
-          <div key={ind} className="text-white space-x-2 text-sm">
-            <span className="text-nowrap">{item.title}</span>
-            <span className="text-primary font-semibold">
-              {rupee.format(item.price)}
-            </span>
-            <span
-              className={cn("text-primary font-semibold", {
-                "text-green-500": item.isUp,
-                "text-red-500": !item.isUp,
-              })}
-            >
-              {item.status()}
-            </span>
-          </div>
-        ))}
-        {popularUnlistedShares.map((item, ind) => (
-          <div key={ind} className="text-white space-x-2 text-sm">
-            <span className="text-nowrap">{item.title}</span>
-            <span className="text-primary font-semibold">
-              {rupee.format(item.price)}
-            </span>
-            <span
-              className={cn("text-primary font-semibold", {
-                "text-green-500": item.isUp,
-                "text-red-500": !item.isUp,
-              })}
-            >
-              {item.status()}
-            </span>
-          </div>
-        ))}
-        {popularUnlistedShares.map((item, ind) => (
-          <div key={ind} className="text-white space-x-2 text-sm">
-            <span className="text-nowrap">{item.title}</span>
-            <span className="text-primary font-semibold">
-              {rupee.format(item.price)}
-            </span>
-            <span
-              className={cn("text-primary font-semibold", {
-                "text-green-500": item.isUp,
-                "text-red-500": !item.isUp,
-              })}
-            >
-              {item.status()}
-            </span>
-          </div>
-        ))}
-      </div>
-    </marquee>
+    <div className="h-10 flex gap-10 items-center bg-black">
+      {Array.from({ length: 5 }).map((item, ind) => (
+        <div
+          key={ind}
+          className="flex shrink-0 animate-marquee flex-row justify-around gap-6"
+        >
+          {popularUnlistedShares.map((item, ind) => (
+            <div key={ind} className="text-white space-x-2 text-sm">
+              <span className="text-nowrap">{item.title}</span>
+              <span className="text-primary font-semibold">
+                {rupee.format(item.price)}
+              </span>
+              <span
+                className={cn("text-primary font-semibold", {
+                  "text-green-500": item.isUp,
+                  "text-red-500": !item.isUp,
+                })}
+              >
+                {item.status()}
+              </span>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
